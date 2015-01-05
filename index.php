06c8437a6ff938ac189334ef495e4ef270d0b879
jklmnn <jklmnn@web.de>
2015-01-05 16:58:27 +0100

    Added server files for Dresden.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$city = $_GET['city'];
+
+if($city == "Dresden"){
+	echo file_get_contents('http://jkliemann.de/offenesdresden.de/json.php');
+}else{
+	echo file_get_contents('meta.json');
+}
+
+?>
