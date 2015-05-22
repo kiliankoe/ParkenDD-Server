97e1b19dea09699aa2956c2bff0d6ff81b41634e
jklmnn <jklmnn@web.de>
2015-05-22 08:54:15 +0200

    Added checking for cities.

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 $city = $_GET['city'];
 
+function getmeta(){
+	$cities = array_values(array_filter(glob('*'), 'is_dir'));
+	$mail = "jklmnn@web.de";
+	$meta = array('mail' => $mail, 'cities' => $cities);
+	return $meta;
+}
+
 if($city === "Dresden"){
 	echo file_get_contents('http://jkliemann.de/offenesdresden.de/json.php');
 }else{
-	echo file_get_contents('meta.json');
+	echo json_encode(getmeta());
 }
 
 ?>
