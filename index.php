75e9a16d8a153c7ecb5bbab1116d5a274995faac
jklmnn <jklmnn@web.de>
2015-05-22 08:53:30 +0200

    (php == php) != (php === php)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $city = $_GET['city'];
 
-if($city == "Dresden"){
+if($city === "Dresden"){
 	echo file_get_contents('http://jkliemann.de/offenesdresden.de/json.php');
 }else{
 	echo file_get_contents('meta.json');
